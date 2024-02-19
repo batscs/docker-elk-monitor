be1ff6e261dab6c5e6cbf05a6cb72172daaa33e9
bats <31670615+batscs@users.noreply.github.com>
2024-02-19 04:39:59 +0100

    Update init.sh

--- app/init.sh
+++ app/init.sh
@@ -1,3 +1,26 @@
-echo "* * * * * python3.11 /app/app.py -v $elastic_domain $elastic_api_key $elastic_index_name > /app/data/app.log" > /app/app.cron
-crontab /app/app.cron
-cron -f
+#!/bin/bash
+
+#echo "* * * * * python3.11 /app/app.py -v $elastic_domain $elastic_api_key $elastic_index_name > /app/data/app.log" > /app/app.cron
+#crontab /app/app.cron
+#cron -f
+
+# Set the command to be executed periodically
+COMMAND="python3.11 /app/app.py $elastic_domain $elastic_api_key $elastic_index_name"
+
+# Set the time interval between each execution (in seconds)
+if [ -n "$interval" ]; then
+    INTERVAL=$interval
+else
+    INTERVAL=10
+fi
+
+# Function to execute the command in a new shell
+execute_command() {
+    bash -c "$COMMAND"
+}
+
+# Infinite loop to periodically execute the command
+while true; do
+    execute_command
+    sleep $INTERVAL
+done
